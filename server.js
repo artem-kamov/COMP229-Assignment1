15124d58e8bdfeb5b08ce2c3a69c65ea9676dbba
artem-kamov <kamov.artyom@gmail.com>
2023-10-07 17:50:41 -0400

    Rendering Views

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-var express = require('./config/express.js');
+var express = require('./config/express');
 var app = express();
 app.listen(3000);
 module.exports = app;
