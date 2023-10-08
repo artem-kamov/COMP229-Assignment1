e549a811240398ccbe16b6e619eda04d9ed6c831
artem-kamov <kamov.artyom@gmail.com>
2023-10-07 20:34:52 -0400

    Routes Creation

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,11 +1,11 @@
 exports.render = function(req, res) { 
-   if (req.session.lastVisit) { 
-         console.log(req.session.lastVisit); 
-      }
-   req.session.lastVisit = new Date();
-    
-   res.render('index', { 
-      title: 'Welcome to Artem\'s website!'
-   }); 
+  if (req.session.lastVisit) { 
+    console.log(req.session.lastVisit); 
+  }
+  req.session.lastVisit = new Date();
+     
+  res.render('index', { 
+    title: 'Artem\'s Website' 
+  }); 
 }; 
     
--- config/express.js
+++ config/express.js
@@ -1,36 +1,36 @@
-var config = require('config'),
-    express = require('express'),
-    morgan = require('morgan'),
-    compress = require('compression'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    session = require('express-session');
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
 
 module.exports = function () {
-var app = express();
-if (process.env.NODE_ENV === 'development') {
-app.use(morgan('dev'));
+  const app = express();
 
-} else if (process.env.NODE_ENV === 'production') {
-app.use(compress());
-}
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compression());
+  }
 
-app.use(bodyParser.urlencoded({
-extended: true
-}));
-app.use(bodyParser.json());
-app.use(methodOverride());
-// The session secret 
-const sessionSecret='developmentSessionSecret';
-app.use(session({
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
+  // Session secret 
+  const sessionSecret='developmentSessionSecret';
+  app.use(session({
     saveUninitialized: true,
     resave: true,
-    secret: config.sessionSecret
-    }));    
-app.set('views', './app/views'),
-app.set('view engine', 'ejs');
-//require('../app/routes/index.server.routes.js')(app);
-app.use(express.static('./public'));
+    secret: sessionSecret
+  }));
 
-return app;
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
+  app.use('/', require('../app/routes/index.server.routes.js'));
+  
+  app.use(express.static('./public'));
+
+  return app;
 };
